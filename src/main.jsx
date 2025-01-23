79ab4657c5f1331ae4f3a3591d1231df945204d5
n-jul <njulkushwaha@gail.com>
2025-01-23 10:33:35 +0530

    Added redux state management, protected the dashboard route. So if user is not authenticated, he will be redirected to signup.

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
-import MainAuthPage from './components/MainAuthPage.jsx'
-import DashboardPage from './components/MainDashboard.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import MainAuthPage from "./components/MainAuthPage.jsx";
+import DashboardPage from "./components/MainDashboard.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import store from "./store/index.js";
+import { Provider } from "react-redux";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path='/' element={<App/>}/>,
-      <Route path='/auth' element={<MainAuthPage/>}/>
-      <Route path='/dashboard' element={<DashboardPage/>}/>
+      <Route path="/" element={<App />} />,
+      <Route path="/auth" element={<MainAuthPage />} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <DashboardPage />
+          </ProtectedRoute>
+        }
+      />
     </>
-    
   )
-)
-createRoot(document.getElementById('root')).render(
+);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
+);
