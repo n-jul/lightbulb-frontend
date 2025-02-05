7ba858e88f330c06dc5b3729f36a79138d339add
n-jul <njulkushwaha@gail.com>
2025-02-05 16:15:08 +0530

    Added campaign seperation in superadmin dashboard too.

--- src/components/CampaignList.jsx
+++ src/components/CampaignList.jsx
@@ -1,11 +1,38 @@
 import CampaignItem from "./CampaignItem";
-
+import { useState } from "react";
 const CampaignList = ({ campaigns, openEditForm, openSendModal }) => {
+  const [activeTab, setActiveTab] = useState("superadmins");
+  const filteredCampaigns =
+    activeTab === "superadmins"
+      ? campaigns.filter((campaign) => campaign.admin_id === null)
+      : campaigns.filter((campaign) => campaign.admin_id !== null);
   return (
     <div className="bg-white shadow rounded-lg p-6">
       <h3 className="text-lg font-medium mb-4">Existing Campaigns</h3>
+      <div className="flex space-x-4 mb-6">
+        <button
+          className={`px-4 py-2 rounded ${
+            activeTab === "superadmins"
+              ? "bg-blue-600 text-white"
+              : "bg-gray-300 text-gray-700"
+          }`}
+          onClick={() => setActiveTab("superadmins")}
+        >
+          Created by Superadmins
+        </button>
+        <button
+          className={`px-4 py-2 rounded ${
+            activeTab === "you"
+              ? "bg-blue-600 text-white"
+              : "bg-gray-300 text-gray-700"
+          }`}
+          onClick={() => setActiveTab("you")}
+        >
+          Created by Admins
+        </button>
+      </div>
       <div className="space-y-4">
-        {campaigns.map((campaign) => (
+        {filteredCampaigns.map((campaign) => (
           <CampaignItem
             key={campaign.id}
             campaign={campaign}
